2614bae76323480647430ff1d04b67838fb1d3f9
wangchengkai <princewck@gmail.com>
2018-09-29 01:56:33 +0800

    update

--- app/service/auth.js
+++ app/service/auth.js
@@ -102,8 +102,8 @@ function _genToken(openid, session_key, userInfo, salt, jwt_private_key, appName
     appName,
   };
   const token = jwt.sign(payload, jwt_private_key, {
-    expiresIn: 5 // 2天后过期
-    // expiresIn: 60 * 60 * 24 * 2 // 2天后过期
+    // expiresIn: 5 // 2天后过期
+    expiresIn: process.env.NODE_ENV === 'production' ? 60 * 60 * 24 * 2 : 30// 2天后过期
   });
   return token;
 }
