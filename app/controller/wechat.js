2b59bb61347ff65106d2257532b9411696494bc1
wangchengkai <princewck@gmail.com>
2018-10-27 04:25:12 +0800

    update

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -7,7 +7,7 @@ module.exports = class WechatController extends Controller {
       const { form_id } = this.ctx.params;
       const header = this.ctx.header;
       if (!form_id) throw new Error('invalid form_id');
-      const token = await this.ctx.service.auth.verify(header['w-session']);    
+      const token = await this.ctx.service.auth.verify(header['w-session']);   
       const user = await this.ctx.service.user.getByToken(token);
       await this.ctx.service.wechat.recordFormId(user.open_id, form_id);
       this.ctx.status = 201;
