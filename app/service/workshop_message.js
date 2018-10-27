70573720796ba30e2a427190494dd9b23eaf5d3c
wangchengkai <princewck@gmail.com>
2018-10-27 23:32:00 +0800

    update

--- app/service/workshop_message.js
+++ app/service/workshop_message.js
@@ -7,6 +7,7 @@ const templateId = 'kZE1seYo0SlPMOcsGhepsGbvlsKpn4bi-7NxTQQuuQ0';
 module.exports = class WorkshopMessageService extends Service {
 
   async dailyRemind() {
+    console.log('-----每日登录提醒:开始发送-----');
     const users = await this.app.mysql.select('daily_remind_list', {
       limit: 10, 
       offset: 0,
@@ -21,17 +22,20 @@ module.exports = class WorkshopMessageService extends Service {
         keyword2: {value: moment().format('YYYY-MM-DD HH:mm')},
         keyword3: {value: '您已经离开24小时啦，是时候记录一下工时和加班信息啦！'},
         keyword4: {value: '上班辛苦啦！'}
-      };      
+      };   
+      console.log(`-----每日登录提醒:第${i + 1}条`);
       const formId = await this.ctx.service.wechat.getAvailableFormId(user.open_id);
+      console.log(`-----每日登录提醒:formId: ${formId}`);
       await this.ctx.service.wechat.sendTemplateMessage(
         accessToken, user.open_id, templateId, '/pages/home', 
         formId, 
         templateData, 
-        emphasisKeyword, 
+        'keyword4.DATA', 
         'workshop',
       );
+      console.log(`-----每日登录提醒:${formId}发送成功！`);
     }
-    return users;
+    console.log(`-----每日登录提醒:本次任务结束！`);
   }
 
 
