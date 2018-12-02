6dc3290a2f4eddeee501325232b1dd33c4a09da2
princewck <princewck@gmail.com>
2018-12-02 16:52:27 +0800

    update

--- app/service/workshop_manage.js
+++ app/service/workshop_manage.js
@@ -81,11 +81,9 @@ module.exports = class WorshopManageService extends Service {
   // 向前兼容旧版设置
   async syncStorageSetting(userId, data) {
     const extra = data.extraCategories || {};
-    const exist = this.app.mysql.get('work_setting', {user_id: userId});
+    const exist = await this.app.mysql.get('work_setting', {user_id: userId});
     if (exist) {
-      console.log('exist', exist);
       this.ctx.logger.warn('配置已存在，同步取消');
-      console.info('配置已存在，同步取消');
       return;
     }
     const conn = this.app.mysql.beginTransaction();
