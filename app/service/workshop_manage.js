e6bbe79e626b4f1b8fb803e2890ce83cc679ec75
princewck <princewck@gmail.com>
2019-01-08 00:15:37 +0800

    update

--- app/service/workshop_manage.js
+++ app/service/workshop_manage.js
@@ -334,6 +334,11 @@ module.exports = class WorshopManageService extends Service {
       end = moment().endOf('month').format('YYYY-MM-DD');
     }
     const setting = await this.getSetting(userId);
+    const pieceSettings = setting.piece_info || [];
+    const pieceSettingMap = pieceSettings.reduce((m, item) => {
+      m[item.id] = item.name;
+      return m;
+    }, {});
     const workDataList = await this.app.mysql.query(`
     select * from work_data where user_id = ? and \`date\` >= ? and \`date\` <= ?
     `, [userId, start, end]);
@@ -343,7 +348,10 @@ module.exports = class WorshopManageService extends Service {
         let extras = [];
         let pieceInfo = [];
         if (setting.calc_method === 'by_count') {
-          pieceInfo = await this.app.mysql.select('work_data_piece', {where: {work_data_id: item.id}});
+          pieceInfo = await this.app.mysql.select('work_data_piece', {where: {work_data_id: item.id}}).toJSON();
+          pieceInfo.forEach(item => {
+            item.name = pieceSettingMap[item.count_setting_id];
+          })
         }
         if (setting.calc_method && setting.calc_method.includes('extra')) {
           extras = await this.app.mysql.select('work_data_extra', {where: {work_data_id: item.id}});
